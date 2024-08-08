ca6ceccac3d2474ccc1aa47337a19d5b8aa0f29c
Harut Harutyunyan <har8unyan@gmail.com>
2024-08-08 09:46:16 +0400

    update sticky note

--- hotbox/commands/Single/StickyNote/002.py
+++ hotbox/commands/Single/StickyNote/002.py
@@ -3,8 +3,11 @@
 # AUTOMATICALLY GENERATED FILE TO BE USED BY W_HOTBOX
 #
 # NAME: Red
+# COLOR: #722626
+# TEXTCOLOR: #111111
 #
 #----------------------------------------------------------------------------------------------------------
 
 for i in nuke.selectedNodes():
     i.knob('tile_color').setValue(3204448511)
+    i.knob('note_font_size').setValue(60)
--- hotbox/commands/Single/StickyNote/003.py
+++ hotbox/commands/Single/StickyNote/003.py
@@ -2,11 +2,12 @@
 #
 # AUTOMATICALLY GENERATED FILE TO BE USED BY W_HOTBOX
 #
-# NAME: Green
-# COLOR: #25591d
+# NAME: Yellow
+# COLOR: #8e6b00
+# TEXTCOLOR: #111111
 #
 #----------------------------------------------------------------------------------------------------------
 
 for i in nuke.selectedNodes():
-    i.knob('tile_color').setValue(10027008)
+    i.knob('tile_color').setValue(4290707711)
     i.knob('note_font_size').setValue(60)
